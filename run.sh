9316969dec33c1f07aed86a2b4a34dea24ccbd48
Chenliang Li <34624799+Cloud0723@users.noreply.github.com>
2023-02-16 23:41:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-nohup python train.py --yaml_file args_yml/model_base_IRL/hopper_v2_medexp.yml --seed 3 --uuid hopper_test1 >>1.out &
-nohup python train.py --yaml_file args_yml/model_base_IRL/halfcheetah_v2_medexp.yml --seed 3 --uuid halfcheetah_test1 >>2.out &
-nohup python train.py --yaml_file args_yml/model_base_IRL/walker2d_v2_medexp.yml --seed 3 --uuid walker_test1 >>3.out &
+python train.py --yaml_file args_yml/model_base_IRL/hopper_v2_medexp.yml --seed 3 --uuid hopper_test1
+python train.py --yaml_file args_yml/model_base_IRL/halfcheetah_v2_medexp.yml --seed 3 --uuid halfcheetah_test1
+python train.py --yaml_file args_yml/model_base_IRL/walker2d_v2_medexp.yml --seed 3 --uuid walker_test1
